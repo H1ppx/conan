3c6b698dfcbe66eaefa34dd2e61d7a377cdc1a1c
root <root@localhost.localdomain>
2024-01-18 22:09:54 +0000

    test2

--- test.py
+++ test.py
@@ -7,7 +7,8 @@ from datasets import load_dataset
 device = "cuda:0" if torch.cuda.is_available() else "cpu"
 torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
-model_id = "distil-whisper/distil-small.en"
+# model_id = "distil-whisper/distil-small.en"
+model_id = "openai/whisper-base.en"
 
 model = AutoModelForSpeechSeq2Seq.from_pretrained(
     model_id, torch_dtype=torch_dtype, low_cpu_mem_usage=True, use_safetensors=True
