6ae2cbe7761c93a9f2df965f47f5c82454670a2e
William Chu <23325016+H1ppx@users.noreply.github.com>
2024-02-27 16:25:50 -0500

    Create nvidia.py

---
+++ nvidia.py
@@ -0,0 +1,32 @@
+import os
+import time
+import nemo.collections.asr as nemo_asr
+from datasets import load_dataset
+
+asr_model = nemo_asr.models.EncDecRNNTBPEModel.from_pretrained(model_name="nvidia/parakeet-tdt-1.1b")
+
+dataset = load_dataset("hf-internal-testing/librispeech_asr_dummy", "clean", split="validation")
+    
+if os.path.exists("results.txt"):
+    os.remove("results.txt")
+f = open("results.txt", "w")
+
+
+for sample in dataset:
+    audio = sample["audio"]
+
+    orig_len = audio["array"]
+    orig_len = len(orig_len)/audio["sampling_rate"]
+
+    start = time.time()
+    result = asr_model.transcribe(['2086-149220-0033.wav'])
+    end = time.time()
+
+    f.write(f"======= Audio ID: {sample['id']} =======\n") 
+    f.write(result[0] + "\n")
+    f.write(f"audio length: {orig_len}\n")
+    f.write(f"Time elapsed: {end - start}\n")
+    f.write(f"Real time factor: {(end - start)/orig_len}\n\n")
+
+f.close()
+    
